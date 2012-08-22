0a756b7620f32a10c054ca6b0ea498412939c71c
Z-MODe <gs@droogc.ru>
2012-08-22 15:17:24 +0400

    push project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Infinityt::Application.config.secret_token = '31c7f15b013810f585e1bcbb8779504caddef1f245449339aa647ce2a2929068b947e06b7ed4e9e82ed594c99fc21daea29c008ffb2c76a8be8eee6b87c32d3d'
