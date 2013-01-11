b43be7f13f962a66685dfa97791ca61c11e17375
maks_home <cot844@yandex.ru>
2013-01-11 19:18:27 +0400

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Infinityt::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
   #config.assets.precompile += %w( *.css *.js )
 
   # Generate digests for assets URLs
