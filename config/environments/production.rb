e78bfea26bad33b8b8efd079da6ee687e048307b
maksim844 <cot844@yandex.ru>
2013-01-17 21:23:55 +0400

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,24 +1,20 @@
 Infinityt::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
-
-  # Code is not reloaded between requests
-  config.cache_classes = true
+ config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
+ # config.assets.precompile += %w( *.css *.js )
   
-  #config.assets.precompile += %w( *.css *.js )
-  #config.assets.precompile = [ /\A[^\/\\]+\.(ccs|js)$/i ]
   # Generate digests for assets URLs
   config.assets.digest = true
 
@@ -26,7 +22,7 @@ Infinityt::Application.configure do
   # config.assets.manifest = YOUR_PATH
 
   # Specifies the header that your server uses for sending files
-   config.action_dispatch.x_sendfile_header = nil # for apache
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
@@ -54,8 +50,7 @@ Infinityt::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Enable threaded mode
-  # config.threadsafe!
-  
+  # config.threadsaf
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
@@ -66,5 +61,5 @@ Infinityt::Application.configure do
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
