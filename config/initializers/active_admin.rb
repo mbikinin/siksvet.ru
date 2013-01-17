5291383a5194ba0df29657506eb20ece9b60e768
maksim844 <cot844@yandex.ru>
2013-01-17 22:57:04 +0400

    fix

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -127,5 +127,5 @@ ActiveAdmin.setup do |config|
   #
   # To load a javascript file:
   #   config.register_javascript 'my_javascript.js'
-   config.register_javascript "/assets/lib/tiny_mce/tiny_mce.js"
+   config.register_javascript "/javascripts/tiny_mce/tiny_mce.js"
 end
