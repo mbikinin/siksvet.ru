105ce71055187b3ecf6dcca7110169c25131d8fe
maksim844 <cot844@yandex.ru>
2013-01-24 00:43:29 +0400

    fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,4 +7,12 @@
 	$(document).ready(function(){
 		$(window).bind('resize', function(){resizeBackground();});
 		$('a[rel*=lazybox]').lazybox();
+		
+			/****
+	 **  Эффект загрузки для remote true
+	 ****/
+		$('a[data-remote],input[data-remote], form[data-remote]')
+		.bind("ajax:beforeSend", function(et, e){ $("#loading").show();	})
+		.bind("ajax:complete", function(et, e){  $("#loading").hide(); })
+		.bind("ajax:after", function(et, e){  $("#loading").hide(); });
 	});
