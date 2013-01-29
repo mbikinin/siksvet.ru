83ff2a4098dab8a6d605d47f7588ef86c8800811
maksim844 <cot844@yandex.ru>
2013-01-29 20:55:34 +0400

    fix

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,8 +1,8 @@
 class RoomsController < ApplicationController
   def index
-    @rooms1 = Room.active.where(:count_rooms => 1).order("RAND(id)").limit(3)
-    @rooms2 = Room.active.where(:count_rooms => 2).order("RAND(id)").limit(3)
-    @rooms3 = Room.active.where(:count_rooms => 3).order("RAND(id)").limit(3)
+    @rooms1 = Room.active.where(:count_rooms => 1).order("RAND()").limit(3)
+    @rooms2 = Room.active.where(:count_rooms => 2).order("RAND()").limit(3)
+    @rooms3 = Room.active.where(:count_rooms => 3).order("RAND()").limit(3)
   end
   def show
     @room = Room.find_by_id(params[:id])
