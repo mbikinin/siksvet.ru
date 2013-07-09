8e296376d1397d89ed1f3cd3a94055fc7057375d
maksim844 <cot844@yandex.ru>
2013-07-09 21:22:33 +0400

    fix

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -8,6 +8,7 @@ class FeedbacksController < ApplicationController
   def create 
     @feedback = Feedback.new(params[:feedback])
     if @feedback.save
+      
        flash[:notice] = "Ваша заявка принята, в ближайшее время наши сотрудники свяжутся с Вами"
      else
        render 'order'
